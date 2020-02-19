56c78e39f0ded4953c6e6f566f84d13ff86dc222
emanuele francioni <emanuele@dusk.network>
2020-02-19 23:39:36 +0100

    short description of download

--- cmd/download.go
+++ cmd/download.go
@@ -24,13 +24,7 @@ import (
 // downloadCmd represents the download command
 var downloadCmd = &cobra.Command{
 	Use:   "download",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "download and configure a Logic repository",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("download called")
 	},
